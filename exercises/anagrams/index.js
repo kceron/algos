06822ec908f55342cfbf07520dbbd3336a9146de
kceron <karemiceron@gmail.com>
2021-02-22 00:41:36 -0500

    1 sol

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,32 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  // build character maps to compare
+  let objA = buildCharMap(stringA);
+  let objB = buildCharMap(stringB);
+
+  // handle edge case where we have different lengths // count the num of keys in both and compare
+  if (Object.keys(objA).length !== Object.keys(objB).length) {
+    return false;
+  }
+  
+  for (let elem in objA) {
+    if (objA[elem] !== objB[elem]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function buildCharMap(str) {
+  const charMap = {};
+
+  // word.replace(/[^\w]/g, "").toLowerCase(); // removes special chars
+  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+  return charMap;
+}
 
 module.exports = anagrams;
