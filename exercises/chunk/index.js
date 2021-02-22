b24d5f217429e15f9726dc907ec32b977ddf3731
kceron <karemiceron@gmail.com>
2021-02-21 23:38:51 -0500

    2 sol

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,29 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let result = [];
+  let index = 0;
+  while (index<array.length) {
+    result.push(array.slice(index, index+size))
+    index+= size
+  }
+  return result;
+}
 
 module.exports = chunk;
+
+// function chunk(array, size) {
+//   let chunked = [];
+   
+//   for(let elem of array){
+//     const last = chunked[chunked.length -1];
+
+//     if (!last || last.length === size) {
+//       chunked.push([elem])
+//     } else {
+//       last.push(elem)
+//     }
+//   }
+//   return chunked;
+// }
