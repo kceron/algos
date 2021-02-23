1d79f6d2ea2236a11ca54de466ef7fe2f408da30
kceron <karemiceron@gmail.com>
2021-02-22 23:25:43 -0500

    my fave solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,31 +9,39 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-  // build character maps to compare
-  let objA = buildCharMap(stringA);
-  let objB = buildCharMap(stringB);
-
-  // handle edge case where we have different lengths // count the num of keys in both and compare
-  if (Object.keys(objA).length !== Object.keys(objB).length) {
-    return false;
-  }
-  
-  for (let elem in objA) {
-    if (objA[elem] !== objB[elem]) {
-      return false;
-    }
-  }
-  return true;
+  return cleanString(stringA) === cleanString(stringB)
 }
 
-function buildCharMap(str) {
-  const charMap = {};
-
-  // word.replace(/[^\w]/g, "").toLowerCase(); // removes special chars
-  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-    charMap[char] = charMap[char] + 1 || 1;
-  }
-  return charMap;
+function cleanString(str){
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
 }
 
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+//   // build character maps to compare
+//   let objA = buildCharMap(stringA);
+//   let objB = buildCharMap(stringB);
+
+//   // handle edge case where we have different lengths // count the num of keys in both and compare
+//   if (Object.keys(objA).length !== Object.keys(objB).length) {
+//     return false;
+//   }
+  
+//   for (let elem in objA) {
+//     if (objA[elem] !== objB[elem]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// function buildCharMap(str) {
+//   const charMap = {};
+
+//   // word.replace(/[^\w]/g, "").toLowerCase(); // removes special chars
+//   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//   return charMap;
+// }
