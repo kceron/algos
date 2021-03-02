f95722763c7e3e78926db52a186257106c00b246
kceron <karemiceron@gmail.com>
2021-03-01 22:18:13 -0500

    recursive solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,19 +17,38 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for(let row=0; row < n; row++){
-    let str = "";
-    
-    for(let column=0; column < n; column++){
-      if(column <= row){
-        str += "#";
-      }else {
-        str += " ";
-      }
-    }
-    console.log(str);
+function steps(n, row = 0, str = '') {
+
+  if (row === n) {
+    return;
   }
+
+  if (str.length === n) {
+    console.log(str);
+    return steps(n, row + 1);
+  }
+
+  if (str.length <= row) {
+    str += '#';
+  } else {
+    str += ' ';
+  }
+  steps(n, row, str);
 }
 
 module.exports = steps;
+
+// function steps(n) {
+//   for(let row=0; row < n; row++){
+//     let str = "";
+    
+//     for(let column=0; column < n; column++){
+//       if(column <= row){
+//         str += "#";
+//       }else {
+//         str += " ";
+//       }
+//     }
+//     console.log(str);
+//   }
+// }
